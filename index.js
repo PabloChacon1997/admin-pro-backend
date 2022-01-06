26bbe9262824666b71b4f71b989a69e6f404b368
PabloChacon1997 <pabloandres120297@gmail.com>
2022-01-06 16:46:49 -0500

    SPA Control

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 
@@ -30,6 +30,12 @@ app.use("/api/hospitales", require("./routes/hospitales"));
 app.use("/api/medicos", require("./routes/medicos"));
 app.use("/api/todo", require("./routes/busquedas"));
 app.use("/api/uploads", require("./routes/uploads"));
+
+// Para deply
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public/index.html"));
+});
 
 app.listen(process.env.PORT, () => {
   console.log("Servidor corriendo en el puerto: " + process.env.PORT);
